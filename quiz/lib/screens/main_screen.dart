906eb2db87cc05103aad1388ad6baafc3388a730
Mussie Alemayehu <mussiealemayehu00@gmail.com>
2024-04-18 19:27:51 +0300

    add main scree

---
+++ quiz/lib/screens/main_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import './quiz_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.of(context).pushNamed(
+            QuizScreen.routeName,
+          ),
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Theme.of(context).colorScheme.onPrimary,
+            backgroundColor: Theme.of(context).primaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+          child: const Text('Take Quiz'),
+        ),
+      ),
+    );
+  }
+}
