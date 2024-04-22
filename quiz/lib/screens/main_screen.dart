ecd6f05b76ae7425ee8cd9e15891acd756c3c461
Mussie Alemayehu <mussiealemayehu00@gmail.com>
2024-04-22 11:03:40 +0300

    improve the UI of the main screen

--- quiz/lib/screens/main_screen.dart
+++ quiz/lib/screens/main_screen.dart
@@ -12,25 +12,202 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  // this is just a dummy main screen, it will be modified to have a more
-  // beautiful UI
+  // a linear gradient that will be used to decorate the clickable containers
+  LinearGradient _linearGradient() {
+    return LinearGradient(
+      begin: Alignment.bottomLeft,
+      end: Alignment.topRight,
+      colors: [
+        Theme.of(context).colorScheme.onPrimary,
+        Theme.of(context).primaryColor.withOpacity(0.4),
+        Theme.of(context).primaryColor.withOpacity(0.1),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.secondary,
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Navigator.of(context).pushNamed(
-            QuizScreen.routeName,
-          ),
-          style: ElevatedButton.styleFrom(
-            foregroundColor: Theme.of(context).colorScheme.onPrimary,
-            backgroundColor: Theme.of(context).primaryColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Quiz time',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      'Choose the amount of questions to start the quiz.',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Theme.of(context).colorScheme.onPrimary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-          child: const Text('Take Quiz'),
+            Expanded(
+              flex: 4,
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(32),
+                    topRight: Radius.circular(32),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    // a row with two elements
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Expanded(
+                            // top-left container - 5 questions
+                            child: InkWell(
+                              onTap: () => Navigator.of(context).pushNamed(
+                                QuizScreen.routeName,
+                                arguments: 5,
+                              ),
+                              child: Container(
+                                width: double.infinity,
+                                height: double.infinity,
+                                decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(32),
+                                  ),
+                                  gradient: _linearGradient(),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '5',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 40,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimary,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            // top-right container - 10 questions
+                            child: InkWell(
+                              onTap: () => Navigator.of(context).pushNamed(
+                                QuizScreen.routeName,
+                                arguments: 10,
+                              ),
+                              child: Container(
+                                width: double.infinity,
+                                height: double.infinity,
+                                decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.only(
+                                    topRight: Radius.circular(32),
+                                  ),
+                                  gradient: _linearGradient(),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '10',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 40,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimary,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // another row with two elements
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Expanded(
+                            // bottom-left container - 15 questions
+                            child: InkWell(
+                              onTap: () => Navigator.of(context).pushNamed(
+                                QuizScreen.routeName,
+                                arguments: 15,
+                              ),
+                              child: Container(
+                                width: double.infinity,
+                                height: double.infinity,
+                                decoration: BoxDecoration(
+                                  gradient: _linearGradient(),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '15',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 40,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimary,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            // bottom-right container - 20 questions
+                            child: InkWell(
+                              onTap: () => Navigator.of(context).pushNamed(
+                                QuizScreen.routeName,
+                                arguments: 20,
+                              ),
+                              child: Container(
+                                width: double.infinity,
+                                height: double.infinity,
+                                decoration: BoxDecoration(
+                                  gradient: _linearGradient(),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '20',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 40,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimary,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
